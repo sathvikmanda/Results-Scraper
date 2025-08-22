635112071ae4fb9cfe6b98cefbe3b1ecacefefb6
Sathvik Manda <112774932+sathvikmanda@users.noreply.github.com>
2025-08-23 02:40:58 +0530

    Update Results.py

--- Results.py
+++ Results.py
@@ -4,13 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 csv_file = "roll.csv"
-url = "https://gradesresults.griet.in/results.php?result=GR22:2024-25:A:320:REGULAR:AUGUST"
+url = "College Website"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 wait = WebDriverWait(driver, 10)
